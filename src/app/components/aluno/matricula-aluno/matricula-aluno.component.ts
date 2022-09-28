2bf67c6f2bfb2a71c223fe1ee9c32a56ed9df5d4
Felipe Almeida <falmeida.df@gmail.com>
2022-09-27 22:34:01 -0300

    validação matricula aluno

--- src/app/components/aluno/matricula-aluno/matricula-aluno.component.ts
+++ src/app/components/aluno/matricula-aluno/matricula-aluno.component.ts
@@ -62,25 +62,6 @@ export class MatriculaAlunoComponent implements OnInit {
     return this.dataUtilService.onMascaraDataInput(event);
   }
 
-  carregarDadosTurma() {
-    // this.matricula.oficinas = [];
-
-    // if (this.matricula.turma.id) {
-    //   this.matricula.turma = _.cloneDeep(_.find(this.turmas, (t: Turmas) => t.id === this.matricula.turma.id));
-
-    //   this.matricula.turma.oficinas.forEach(oficina => {
-
-    //     const atividade = new AtividadeAluno();
-    //     atividade.dataInicioAtividade = this.matricula.dataInicio;
-    //     atividade.dataDesvinculacao   = this.matricula.dataDesvinculacao;
-    //     atividade.aluno               = this.matricula.aluno;
-    //     atividade.atividade           = oficina;
-
-    //     this.matricula.oficinas.push(atividade);
-    //   })
-    // }
-  }
-
   deletar(oficina: AtividadeAluno, indexMatricula: number, indexOficina: number) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.data = {
@@ -109,5 +90,135 @@ export class MatriculaAlunoComponent implements OnInit {
         dialogRef.close();
       }
     });
+  }
+
+  adicionarTurma() {
+    const matricula = new AlunosTurma();
+    matricula.oficinas = [];
+
+    if (this.turma.id) {
+      matricula.turma = _.cloneDeep(_.find(this.turmas, (t: Turmas) => t.id === this.turma.id));
+      matricula.dataInicio = this.dataInicioMatricula;
+      matricula.turma.oficinas.forEach(oficina => {
+
+        const atividade = new AtividadeAluno();
+        atividade.dataInicioAtividade = this.dataInicioMatricula;
+        atividade.atividade           = oficina;
+
+        matricula.oficinas.push(atividade);
+      })
+    }
+    const oficinasSemConfiltos = this.validarDatasInicioMatriculadasNaTurma(matricula);
+    if(oficinasSemConfiltos && oficinasSemConfiltos.length > 0) {
+      const index = this.matriculas.findIndex(item => item.turma.id === matricula.turma.id);
+      if(index >= 0) {
+        this.matriculas[index].oficinas.push(...oficinasSemConfiltos);
+      } else {
+        matricula.oficinas = oficinasSemConfiltos;
+        this.matriculas.push(matricula);
+      }
+    }
+  }
+
+  validarDatasInicioMatriculadasNaTurma(matricula: AlunosTurma) {
+
+    const dataInicioMatricula = this.dataUtilService.getDataTruncata(matricula.dataInicio);
+    const dataFimMatricula    = this.dataUtilService.getDataTruncata(matricula.dataDesvinculacao);
+    const dataInicioTurma     = this.dataUtilService.getDataTruncata(matricula.turma.dataInicioTurma);
+    const oficinas = [];
+
+    if(dataFimMatricula && dataInicioMatricula.getTime() > dataFimMatricula.getTime()) {
+      this.toastService.showAlerta('A data de fim da matrícula tem que maior ou igual à data de início.');
+      return;
+    }
+
+    if(dataInicioMatricula.getTime() < dataInicioTurma.getTime()) {
+      this.toastService.showAlerta('A data de início da matricula (' + dataInicioMatricula.toLocaleDateString() 
+                                   +') é menor que a data de início da turma ('+ dataInicioTurma.toLocaleDateString() +')');
+      return;
+    }
+
+    //Não permite que a data de início das oficinas sejam menores que a data de inicio da matrícula da turma
+    if (matricula.oficinas) {
+      matricula.oficinas.forEach(oficina => {
+        let dataValida = true;
+        const dataInicio = this.dataUtilService.getDataTruncata(oficina.dataInicioAtividade);
+        const dataFim    = this.dataUtilService.getDataTruncata(oficina.dataDesvinculacao);
+
+        
+        if (dataInicio && dataFimMatricula && dataInicio.getTime() > dataFimMatricula.getTime()) {
+          dataValida = false;
+        }
+
+        if( (!dataFim && dataFimMatricula) ||
+            (dataFimMatricula && dataFim.getTime() > dataFimMatricula.getTime())) {
+          dataValida = false;
+        }
+
+        if (dataInicio.getTime() < dataInicioMatricula.getTime()) {
+           dataValida = false;
+        }
+
+        //Valida conflitos entres oficinas da turma
+        const matriculasMesmaOficina = matricula.oficinas.filter(o => o.atividade.id === oficina.atividade.id && oficina.id != o.id);
+        if(matriculasMesmaOficina) {
+          matriculasMesmaOficina.forEach(m => {
+            const matriculaConflito = this.dataUtilService.isEntreDatasTruncada(oficina.dataInicioAtividade, oficina.dataDesvinculacao, m.dataInicioAtividade, m.dataDesvinculacao);
+            if(matriculasMesmaOficina) {
+              dataValida = false;
+            }            
+          })
+        } 
+        if(dataValida) {
+          oficinas.push(oficina);
+        }
+
+      });
+      return this.recuperaOficinasSemConflito(oficinas);
+    }
+  }
+
+  private recuperaOficinasSemConflito(matriculas: AtividadeAluno[]) {
+    const oficinasComConflitos: AtividadeAluno[] = [];
+    const oficinasSemConflitos: AtividadeAluno[] = [];
+    this.matriculas.forEach(matricula => {
+      const matriculasTurma   = matricula.oficinas;
+      const outrasMatriculas  = matriculas;
+      
+      matriculasTurma.forEach(oficina => {
+        let outrasMatriculasFiltrada = outrasMatriculas;
+        if(oficina.id) {
+          outrasMatriculasFiltrada = outrasMatriculas.filter(item => item.id !== oficina.id);
+        }
+        let temp: any[] = outrasMatriculasFiltrada.filter(om => ((om.atividade.domingo && oficina.atividade.domingo) || 
+                                                         (om.atividade.segunda && oficina.atividade.segunda) || 
+                                                         (om.atividade.terca   && oficina.atividade.terca)   || 
+                                                         (om.atividade.quarta  && oficina.atividade.quarta)  || 
+                                                         (om.atividade.quinta  && oficina.atividade.quinta)  || 
+                                                         (om.atividade.sexta   && oficina.atividade.sexta)   || 
+                                                         (om.atividade.sabado  && oficina.atividade.sabado)) 
+                                                        &&
+                                                        (
+                                                          oficina.atividade.horaInicio >= om.atividade.horaInicio && oficina.atividade.horaInicio <= om.atividade.horaFim 
+                                                          || 
+                                                          oficina.atividade.horaFim >= om.atividade.horaInicio && oficina.atividade.horaFim <= om.atividade.horaFim
+                                                        )
+                                                        &&
+                                                        this.dataUtilService.isEntreDatasTruncada( oficina.dataInicioAtividade, oficina.dataDesvinculacao, om.dataInicioAtividade, om.dataDesvinculacao)
+                                                        );  
+  
+        
+        if(temp.length > 0) {
+          oficinasComConflitos.push(oficina)
+        }                                         
+      });
+    });
+    matriculas.forEach(item => {
+      const possuiConflito = oficinasComConflitos.filter(itemOficina => itemOficina.atividade.id === item.atividade.id);
+      if(!possuiConflito || possuiConflito.length === 0) {
+        oficinasSemConflitos.push(item);
+      }
+    })
+    return oficinasSemConflitos;
   }
 }
